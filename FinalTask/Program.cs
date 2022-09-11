59053cac6d8ab67132c131bec28b6a770a988535
Alexandr Antonov <sasha98antonov05@gmail.com>
2022-09-12 01:26:43 +0300

    Написал рабочее решение задачи для вывода массива строк

--- FinalTask/Program.cs
+++ FinalTask/Program.cs
@@ -1,1 +1,19 @@
 ﻿// Написать программу, которая из имеющегося массива строк формирует массив из строк, длина которых меньше или равна 3 символа.
+
+string[] city = {"Moscow", "Hamburg", "Munchen", "Drezden", "Minsk", "Ufa", "Bor", "Loo", "x2"};
+int i = 0;
+int j = 0;
+string[] city2 = new string[10];
+while(i < city.Length)
+{
+    if(city[i].Length <= 3)
+    {
+       city2[j] = city[i] + " ";
+    }
+    i++;
+    j++;
+}
+foreach(string str in city2)
+{
+    Console.Write(str);
+}
